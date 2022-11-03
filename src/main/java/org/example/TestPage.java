d1255f57e5b51120690c0f5547bbe232fcb09e7f
TatianaTelnova <tanat1900tana@gmail.com>
2022-11-03 13:02:59 +0300

    second commit

--- src/main/java/org/example/TestPage.java
+++ src/main/java/org/example/TestPage.java
@@ -1,2 +1,11 @@
-package org.example;public class TestPage {
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestPage {
+    protected WebDriver driver;
+
+    public TestPage(WebDriver driver) {
+        this.driver = driver;
+    }
 }
--- src/test/java/Test_TestPage.java
+++ src/test/java/Test_TestPage.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Test_TestPage {
+import org.example.TestPage;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class Test_TestPage {
+    protected WebDriver driver;
+    @Test
+    public void firstTest() {
+        new TestPage(driver);
+    }
 }
